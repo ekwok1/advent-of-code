7727e841c3b145b9884069129906c9339c1f202d
Eugene Kwok <ekwok@relias.com>
2021-12-01 12:17:42 -0500

    write tests for test cases

---
+++ 01_sonar-sweep/sonar-sweep_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ekwok1/aoc-2021/utilities"
+)
+
+func TestCountIncreases(t *testing.T) {
+	file, measurements := utilities.ScanInts("test-input.txt")
+	defer file.Close()
+
+	increases := countIncreases(measurements)
+	if increases != 7 {
+		t.Errorf("countIncreases(measurements) = %d; want 7", increases)
+	}
+}
+
+func TestCountSlidingIncreases(t *testing.T) {
+	file, measurements := utilities.ScanInts("test-input.txt")
+	defer file.Close()
+
+	increases := countSlidingIncreases(measurements)
+	if increases != 5 {
+		t.Errorf("countSlidingIncreases(measurements) = %d; want 5", increases)
+	}
+}
