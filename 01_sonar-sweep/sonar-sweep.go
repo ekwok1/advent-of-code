0c4ec9776748521df7b0ab93eeb5803caccf76e6
Eugene Kwok <ekwok@relias.com>
2021-12-01 10:46:53 -0500

    day 1 puzzle 2

--- 01_sonar-sweep/sonar-sweep.go
+++ 01_sonar-sweep/sonar-sweep.go
@@ -9,27 +9,34 @@ import (
 )
 
 func main() {
-	file, _ := os.Open("test-input.txt")
+	file, _ := os.Open(os.Args[1])
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	scanner.Split(bufio.ScanLines)
-	var measurements []int64
+
+	var measurements []int
 
 	for scanner.Scan() {
 		i, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-		measurements = append(measurements, i)
+		measurements = append(measurements, int(i))
+	}
+
+	var slidingMeasurements []int
+
+	for i := 0; i < len(measurements)-2; i++ {
+		sum := measurements[i] + measurements[i+1] + measurements[i+2]
+		slidingMeasurements = append(slidingMeasurements, sum)
 	}
 
 	previous := math.MaxInt
 	increases := 0
-	for _, measurement := range measurements {
-		if int(measurement) > previous {
+	for _, slidingMeasurement := range slidingMeasurements {
+		if slidingMeasurement > previous {
 			increases = increases + 1
 		}
 
-		previous = int(measurement)
+		previous = int(slidingMeasurement)
 	}
 
 	fmt.Println(increases)
