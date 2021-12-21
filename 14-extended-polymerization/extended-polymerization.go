05dfec50924be30f6433048dac238dfd235b6ac6
Eugene Kwok <ekwok@relias.com>
2021-12-21 17:43:46 -0500

    day 14 add tests

--- 14-extended-polymerization/extended-polymerization.go
+++ 14-extended-polymerization/extended-polymerization.go
@@ -13,49 +13,23 @@ func main() {
 	file, polymerData := utilities.ScanStringsFromFile(os.Args[1])
 	defer file.Close()
 
-	polymerInsertionRules := polymerData[1:]
-	polymerInsertionRuleMap := make(map[string]string)
-	for _, rule := range polymerInsertionRules {
-		insertionRule := strings.Split(rule, " -> ")
-		polymerInsertionRuleMap[insertionRule[0]] = insertionRule[1]
-	}
+	polymerInsertionRuleList := polymerData[1:]
+	polymerInsertionRules := initializePolymerInsertionRuleMap(&polymerInsertionRuleList)
 
-	elementCounter := make(map[string]int)
 	polymerTemplate := polymerData[0]
-	polymerTracker := make(map[string]int)
+	elementCounter, polymerTracker := initializeCounterAndTracker(&polymerTemplate)
 
-	for i := 0; i < len(polymerTemplate)-1; i++ {
-		element1 := string(polymerTemplate[i])
-		element2 := string(polymerTemplate[i+1])
-		elementCounter[element1]++
-		if i == len(polymerTemplate)-2 {
-			elementCounter[element2]++
-		}
+	steps := 40
+	step(steps, &elementCounter, &polymerTracker, &polymerInsertionRules)
 
-		pair := element1 + element2
-		polymerTracker[pair]++
-	}
+	diff := calculateGreatestDiff(&elementCounter)
+	fmt.Println("Diff:", diff)
+}
 
-	for steps := 0; steps < 40; steps++ {
-		newPolymerTracker := make(map[string]int)
-
-		for k, v := range polymerTracker {
-			insert := polymerInsertionRuleMap[k]
-			elementCounter[insert] += v
-
-			newPairLeft := string(k[0]) + insert
-			newPairRight := insert + string(k[1])
-
-			newPolymerTracker[newPairLeft] += v
-			newPolymerTracker[newPairRight] += v
-		}
-
-		polymerTracker = newPolymerTracker
-	}
-
+func calculateGreatestDiff(elementCounter *map[string]int) int {
 	mostCommon := math.MinInt
 	leastCommon := math.MaxInt
-	for _, v := range elementCounter {
+	for _, v := range *elementCounter {
 		if v > mostCommon {
 			mostCommon = v
 		} else if v < leastCommon {
@@ -63,33 +37,55 @@ func main() {
 		}
 	}
 
-	fmt.Println(mostCommon)
-	fmt.Println(leastCommon)
+	return mostCommon - leastCommon
+}
 
-	fmt.Println(mostCommon - leastCommon)
+func step(steps int, elementCounter, polymerTracker *map[string]int, polymerInsertionRules *map[string]string) {
+	for s := 0; s < steps; s++ {
+		newPolymerTracker := make(map[string]int)
 
-	// NAIVE SOLUTION
-	// polymerTemplate := polymerData[0]
-	// polymer := polymerTemplate
-	// newPolymer := ""
-	// for step := 0; step < 10; step++ {
-	// 	for i := 0; i < len(polymer)-1; i++ {
-	// 		pair := string(polymer[i]) + string(polymer[i+1])
-	// 		insert := polymerInsertionRuleMap[pair]
+		for polymer, count := range *polymerTracker {
+			insert := (*polymerInsertionRules)[polymer]
+			(*elementCounter)[insert] += count
 
-	// 		newPolymer += string(polymer[i]) + insert
-	// 		if i == len(polymer)-2 {
-	// 			newPolymer += string(polymer[i+1])
-	// 		}
-	// 	}
-	// 	polymer = newPolymer
-	// 	newPolymer = ""
-	// }
+			newPairLeft := string(polymer[0]) + insert
+			newPairRight := insert + string(polymer[1])
 
-	// polymerCountMap := make(map[string]int)
-	// for i := 0; i < len(polymer); i++ {
-	// 	polymerCountMap[string(polymer[i])]++
-	// }
+			newPolymerTracker[newPairLeft] += count
+			newPolymerTracker[newPairRight] += count
+		}
 
-	// fmt.Println(polymerCountMap)
+		polymerTracker = &newPolymerTracker
+	}
+}
+
+func initializeCounterAndTracker(polymerTemplate *string) (map[string]int, map[string]int) {
+	elementCounter := make(map[string]int)
+	polymerTracker := make(map[string]int)
+
+	for i := 0; i < len(*polymerTemplate)-1; i++ {
+		element1 := string((*polymerTemplate)[i])
+		element2 := string((*polymerTemplate)[i+1])
+
+		elementCounter[element1]++
+		if i == len((*polymerTemplate))-2 {
+			elementCounter[element2]++
+		}
+
+		polymerTracker[element1+element2]++
+	}
+
+	return elementCounter, polymerTracker
+}
+
+func initializePolymerInsertionRuleMap(polymerInsertionRuleList *[]string) map[string]string {
+	polymerInsertionRules := make(map[string]string)
+
+	for _, rule := range *polymerInsertionRuleList {
+		fromTo := strings.Split(rule, " -> ")
+		from, to := fromTo[0], fromTo[1]
+		polymerInsertionRules[from] = to
+	}
+
+	return polymerInsertionRules
 }
